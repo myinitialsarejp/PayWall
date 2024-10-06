101c44d9c77d8474b07c0a6df1c5a9c4bfb4cd3c
Jesse Pelletier <jespel2013@gmail.com>
2024-10-06 11:06:52 -0700

    added mui, paper, etc

--- paywall/src/App.js
+++ paywall/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainWindow from './Components/MainWindow';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <MainWindow />
       </header>
     </div>
   );
--- paywall/src/Components/MainWindow.js
+++ paywall/src/Components/MainWindow.js
@@ -1,10 +1,14 @@
+import { Paper, Typography } from '@mui/material'
 import * as React from 'react'
+import PaperBackground from '../Assets/white-paper-texture.jpg'
 
 const MainWindow = (props) => {
 
     return (
-        <>
-        </>
+
+        <Paper elevation={24} sx={{padding: '20%', backgroundImage: `url(${PaperBackground})`}}>
+            <Typography>Papier</Typography>
+        </Paper>
     )
 }
 
